a0e14c3f5fc8e476062806f7cd2dd3610ab199db
Cataldo Calò <cataldo.calo@gmail.com>
2019-06-18 15:18:53 +0200

    Forgot to fetch meta-data

--- repo/repo.py
+++ repo/repo.py
@@ -18,13 +18,16 @@ def clone_repo(https_url, git_url):
     empty_repo = git.Repo.init(path=repo_path, mkdir=True)
 
     # origin = empty_repo.create_remote('origin', https_url)
-    origin = empty_repo.create_remote('origin', git_url)
+    origin = empty_repo.create_remote('origin', https_url)
     assert origin.exists()
     assert origin == empty_repo.remotes.origin == empty_repo.remotes['origin']
 
+    # Download repo meta-data from remote
+    origin.fetch()
     # Setup a local tracking branch of a remote branch
     empty_repo.create_head('master', origin.refs.master)  # create local branch "master" from remote "master"
     empty_repo.heads.master.set_tracking_branch(origin.refs.master)  # set local "master" to track remote "master
     empty_repo.heads.master.checkout()  # checkout local "master" to working tree
 
+    # Pull repository
     origin.pull()
