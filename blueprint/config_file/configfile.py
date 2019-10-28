72d17aca173793695fad2aad251cd5f7677e7475
Cataldo Calò <cataldo.calo@gmail.com>
2019-10-28 22:02:14 +0100

    Handled exceptions for illegal value of paths

--- blueprint/config_file/configfile.py
+++ blueprint/config_file/configfile.py
@@ -11,10 +11,13 @@ class ConfigFile(ABC):
         with open(self.get_path(file_conf_path, None)) as config:
             self.config_file = json.load(config)
 
-    def get_path(self, filepath, section):
-        if filepath == '':
+    def get_path(self, relative_path, section):
+        if relative_path == '' or relative_path == '/':
+            raise InvalidRootDirectory(section, relative_path)
+        abs_path = os.path.abspath(os.path.join(self.repo_path, relative_path))
+        if not os.path.isdir(abs_path) and not os.path.isfile(abs_path):
             raise MissingReferenceException(section)
-        return os.path.join(self.repo_path, filepath)
+        return os.path.normpath(abs_path)
 
     def get_section(self, section):
         return self.config_file[section]
@@ -28,6 +31,15 @@ class MissingReferenceException(Exception):
         self.section = section
 
     def print(self, config_file):
-        print('Missing ' + self.section + ' path in file ' + config_file)
+        print('ERROR: Missing ' + self.section + ' path in file ' + config_file)
 
 
+class InvalidRootDirectory(Exception):
+    def __init__(self, attribute, illegal_path):
+        self.attribute = attribute
+        self.illegal_path = illegal_path
+
+    def print(self, config_file):
+        description = 'ERROR: "' + self.illegal_path + '" invalid root directory value for attribute "' + \
+                      self.attribute + '" in "' + config_file + '". Use "./" instead.'
+        print(description)
